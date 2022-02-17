7bdb9fedee2ae352aad982126ce9b195278b6429
彭月祥 <1653365777@qq.com>
2022-02-17 15:28:58 +0800

    00000

--- src/background.js
+++ src/background.js
@@ -1,6 +1,9 @@
 'use strict'
 
 import { app, protocol, BrowserWindow, dialog } from 'electron'
+
+const { autoUpdater } = require('electron-updater')
+
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
@@ -9,6 +12,53 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
+
+
+// ---------------------------------
+//检测更新
+autoUpdater.checkForUpdates()
+
+async function checkUpdate() {
+  if(process.platform == 'darwin'){  
+    //我们使用koa-static将静态目录设置成了static文件夹，
+    //所以访问http://127.0.0.1:9005/darwin，就相当于访问了static/darwin文件夹，win32同理
+    autoUpdater.setFeedURL('http://127.0.0.1:9005/darwin')  //设置要检测更新的路径
+    
+  }else{
+    autoUpdater.setFeedURL('http://127.0.0.1:9005/win')
+  }
+}
+
+//监听'error'事件
+autoUpdater.on('error', (err) => {
+  console.log(err)
+})
+
+//监听'update-available'事件，发现有新版本时触发
+autoUpdater.on('update-available', () => {
+  console.log('found new version')
+})
+
+ //默认会自动下载新版本，如果不想自动下载，设置autoUpdater.autoDownload = false
+  
+  //监听'update-downloaded'事件，新版本下载完成时触发
+  autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: '应用更新',
+      message: '发现新版本，是否更新？',
+      buttons: ['是', '否']
+    }).then((buttonIndex) => {
+      if(buttonIndex.response == 0) {  //选择是，则退出程序，安装新版本
+        autoUpdater.quitAndInstall() 
+        app.quit()
+      }else {
+        
+      }
+    })
+  })
+
+// ---------------------------------
 
 async function createWindow() {
   console.log(process.versions);
@@ -55,6 +105,7 @@ app.on('activate', () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
+  checkUpdate()
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
--- vue.config.js
+++ vue.config.js
@@ -1,108 +1,61 @@
 module.exports = {
-    runtimeCompiler: true,
+  runtimeCompiler: true,
 
-    pluginOptions: {
-        electronBuilder: {
-          builderOptions: {
-            // options placed here will be merged with default configuration and passed to electron-builder
-            appId: "this.is.tasky",
-            productName: "desktop",
-            copyright: "Copyright © 2021 Alaso",
-            directories: {
-              buildResources: "build"
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+        appId: "this.is.tasky",
+        productName: "desktop",
+        copyright: "Copyright © 2021 Alaso",
+        directories: {
+          buildResources: "build"
+        },
+        mac: {
+          category: "public.app-category.utilities"
+        },
+        dmg: {
+          icon: "build/icons/icon.icns",
+          iconSize: 100,
+          contents: [
+            {
+              x: 380,
+              y: 180,
+              type: "link",
+              path: "/Applications"
             },
-            mac: {
-              category: "public.app-category.utilities"
-            },
-            dmg: {
-              icon: "build/icons/icon.icns",
-              iconSize: 100,
-              contents: [
-                {
-                  x: 380,
-                  y: 180,
-                  type: "link",
-                  path: "/Applications"
-                },
-                {
-                  x: 130,
-                  y: 180,
-                  type: "file"
-                }
-              ],
-              window: {
-                width: 540,
-                height: 380
-              }
-            },
-            win: {
-              target: [
-                'msi',
-                'nsis'
-              ],
-              icon: "build/icons/icon.ico"
-            },
-            nsis: {
-              oneClick: false,
-              language: "2052",
-              perMachine: true,
-              allowToChangeInstallationDirectory: true
+            {
+              x: 130,
+              y: 180,
+              type: "file"
             }
+          ],
+          window: {
+            width: 540,
+            height: 380
           }
-        }
+        },
+        win: {
+          target: [
+            'msi',
+            'nsis'
+          ],
+          icon: "build/icons/icon.ico"
+        },
+        nsis: {
+          oneClick: false,
+          language: "2052",
+          perMachine: true,
+          allowToChangeInstallationDirectory: true
+        },
+        publish:[
+          {
+            provider: "generic",
+            url: "http://127.0.0.1:9005/" 
+          }
+        ]
       }
-    // pluginOptions: {
-    //     electronBuilder: {
-    //         builderOptions: {
-    //             // build配置在此处
-    //             // options placed here will be merged with default configuration and passed to electron-builder
-
-    //             build: {
-    //                 "appId": "your.app.id",
-    //                 "productName": "Tasky",
-    //                 "copyright": "Copyright © 2021 Alaso",
-    //                 "directories": {
-    //                     "buildResources": "build",
-    //                     "output": "dist"
-    //                 },
-    //                 "win": {
-    //                     "target": ["msi", "nsis"],
-    //                     "icon": "build/icons/icon.ico"
-    //                 },
-    //                 "nsis": {
-    //                     "oneClick": false,
-    //                     "language": "2052",
-    //                     "perMachine": true,
-    //                     "allowToChangeInstallationDirectory": true
-    //                 },
-    //                 "mac": {
-    //                     "target": ["dmg", "zip"],
-    //                     "category": "public.app-category.utilities"
-    //                 },
-    //                 "dmg": {
-    //                     "icon": "build/icons/icon.icns",
-    //                     "iconSize": 100,
-    //                     "contents": [
-    //                         {
-    //                             "x": 380,
-    //                             "y": 180,
-    //                             "type": "link",
-    //                             "path": "/Applications"
-    //                         },
-    //                         {
-    //                             "x": 130,
-    //                             "y": 180,
-    //                             "type": "file"
-    //                         }
-    //                     ],
-    //                     "window": {
-    //                         "width": 540,
-    //                         "height": 380
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     },
-    // },
+    }
+  }
 }
 
